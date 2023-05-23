d34c7605906b6643e9e03dd04a11d60968366918
shihong <shihong@jimeng.io>
2023-05-23 15:08:24 +0800

    feat(debug): 增强调试能力

--- packages/core/src/utils/debugger.js
+++ packages/core/src/utils/debugger.js
@@ -4,19 +4,16 @@ window._wpm_debugger_filters = () => {
 
 const logStyle_1 =
   'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff'
-const logStyle_2 =
-  'background:#1677ff ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff'
 const logStyle_3 =
   'background:#41b883 ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff'
 
 export function debugLogger(moduleName, functionKey, args, result) {
   if (!window._wpm_debugger_filters(moduleName, functionKey, args, result)) {
-    console.debug('%c 请求::=> ', logStyle_1)
     console.debug(
-      `%c 模块名/方法:: ${moduleName}/${functionKey} => 参数::=>`,
-      logStyle_2,
+      `%c 请求::=> ${moduleName}/${functionKey} 参数::`,
+      logStyle_1
+    ),
       args
-    )
     result && console.debug('%c 响应::=>', logStyle_3, result)
     console.debug('<-------------------->')
   }
