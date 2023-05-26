8128388d5cd36f0889167bc64014ec6fec058a20
shihong <shihong@jimeng.io>
2023-05-26 10:48:37 +0800

    feat(debug): 增强调试能力

--- packages/core/src/utils/debugger.js
+++ packages/core/src/utils/debugger.js
@@ -10,15 +10,15 @@ export function debugLogger(moduleName, functionKey, args, result) {
   if (!window._wpm_debugger_filters(moduleName, functionKey, args, result)) {
     if (!result) {
       console.debug(
-        `%c 请求::=> ${moduleName}.${functionKey} 参数::`,
+        `%c 请求::=> ${moduleName}.${functionKey} 请求参数::`,
         logStyle_1,
-        args
+        args.length > 0 ? [...args] : '无'
       )
     } else {
       console.debug(
-        `%c 响应::=> ${moduleName}.${functionKey} 参数::`,
+        `%c 响应::=> ${moduleName}.${functionKey}`,
         logStyle_3,
-        args,
+        '返回结果:',
         result
       )
     }
